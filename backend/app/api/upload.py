fdace7ed989f2c2492624b26bc4a5d08ca664fd7
PixelCraftLab <vishalgowdask65@gmail.com>
2026-07-13 17:07:41 +0530

    pdf upload, verify and saving compleated

--- backend/app/api/upload.py
+++ backend/app/api/upload.py
@@ -16,6 +16,23 @@ UPLOAD_DIRECTORY.mkdir(
 def upload_pdf(
     file: UploadFile = File(...)
 ):
+    if not file.filename.lower().endswith(".pdf"):
+        raise HTTPException(
+        status_code=400,
+        detail="Only PDF files are allowed."
+    )
+    file_path = UPLOAD_DIRECTORY / file.filename
+
+    if file_path.exists():
+        raise HTTPException(
+        status_code=409,
+        detail="Document already exists."
+    )
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(
+        file.file,
+        buffer,
+    )
     return {
         "filename": file.filename
     }
